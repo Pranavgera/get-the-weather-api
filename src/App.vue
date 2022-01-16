071f1993833188e7669cae8de342623483f79520
Pranav Gera <Pranav.gera@dcmail.ca>
2022-01-15 21:11:16 -0500

    Imported materialize css for the devlopment

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main id="app">
+    <div class="weatherContainer l8 Red">
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import M from "materialize-css";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+
+mounted() {
+    M.AutoInit();
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
