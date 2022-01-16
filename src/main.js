694b616b5a1e9a626f2f682884c6fbf142785ff9
Pranav Gera <Pranav.gera@dcmail.ca>
2022-01-15 21:10:43 -0500

    Added assets for the Weather App

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueMaterialTabs from 'vue-material-tabs';
 
-createApp(App).mount('#app')
+import AxiosPlugin from 'vue-axios-cors';
+
+import 'materialize-css/dist/css/materialize.css'
+import 'material-design-icons/iconfont/material-icons.css'
+
+
+Vue.config.productionTip = false
+Vue.use(VueMaterialTabs);
+
+Vue.use(AxiosPlugin)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
